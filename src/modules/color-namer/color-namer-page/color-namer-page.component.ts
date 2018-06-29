513514727b1971725984467141db1562dba0ea15
Simon Lepel <simbo@simbo.de>
2018-06-29 13:49:57 +0200

    refactor component templates to pug

--- src/modules/color-namer/color-namer-page/color-namer-page.component.ts
+++ src/modules/color-namer/color-namer-page/color-namer-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'color-namer-page',
-  templateUrl: './color-namer-page.component.html',
+  templateUrl: './color-namer-page.component.pug',
   styleUrls: ['./color-namer-page.component.scss']
 })
 export class ColorNamerPageComponent implements OnInit {
--- src/modules/hash-generator/hash-generator-page/hash-generator-page.component.ts
+++ src/modules/hash-generator/hash-generator-page/hash-generator-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'hash-generator-page',
-  templateUrl: './hash-generator-page.component.html',
+  templateUrl: './hash-generator-page.component.pug',
   styleUrls: ['./hash-generator-page.component.scss']
 })
 export class HashGeneratorPageComponent implements OnInit {
