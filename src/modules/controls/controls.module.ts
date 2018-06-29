dad1d6867970cb460d4ecf14c20e839cbbb86a28
Simon Lepel <simbo@simbo.de>
2018-06-30 00:00:24 +0200

    implement basic textfield component

--- src/modules/controls/controls.module.ts
+++ src/modules/controls/controls.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+import { TextfieldComponent } from './textfield/textfield.component';
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule
   ],
   declarations: [
+    TextfieldComponent
   ],
   exports: [
-    FormsModule
+    FormsModule,
+    TextfieldComponent
   ]
 })
 export class ControlsModule { }
