38f3bd314aa3d597434229c600f76cac73eba69e
Simon Lepel <simbo@simbo.de>
2018-06-29 14:46:27 +0200

    add hash helpers to hash-generator module

---
+++ src/modules/hash-generator/hash/hash-encodings.ts
@@ -0,0 +1,11 @@
+export enum HashEncoding {
+  Hex = 'hex',
+  Base64 = 'base64',
+  Latin1 = 'latin1'
+}
+
+export const hashEncodings = {
+  Hex: HashEncoding.Hex,
+  Base64: HashEncoding.Base64,
+  Latin1: HashEncoding.Latin1
+};
