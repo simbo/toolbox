e53c55856df62d16b502a9f727aa414e86f55c00
Simon Lepel <simbo@simbo.de>
2018-06-29 14:47:08 +0200

    fix app component tests

--- src/modules/app/app.component.spec.ts
+++ src/modules/app/app.component.spec.ts
@@ -1,4 +1,6 @@
 import { TestBed, async } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -8,6 +10,7 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     }).compileComponents();
   }));
 
