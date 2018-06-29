7fe32f9480cb59c452bc1b31fa142618625dcf84
Simon Lepel <simbo@simbo.de>
2018-06-29 15:55:35 +0200

    refactor home-page template to pug; remove unused oninit

--- src/modules/app/home-page/home-page.component.ts
+++ src/modules/app/home-page/home-page.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'home-page',
-  templateUrl: './home-page.component.html',
+  templateUrl: './home-page.component.pug',
   styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class HomePageComponent {
 
 }
