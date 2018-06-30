46a8f31371ae25f25de9c7c9885f1ca5977133d3
Simon Lepel <simbo@simbo.de>
2018-06-30 14:57:06 +0200

    small refactoring on hash-generator-page

--- src/modules/hash-generator/hash-generator-page/hash-generator-page.component.ts
+++ src/modules/hash-generator/hash-generator-page/hash-generator-page.component.ts
@@ -5,6 +5,21 @@ import { hashEncodings, HashEncoding } from '../hash/hash-encodings';
 import { Hash } from '../hash/hash';
 import { SelectChoices } from '../../controls/select/select.component';
 
+
+const algorithmChoices: SelectChoices = Object.entries(hashAlgorithms)
+  .reduce((algorithms, [label, value]) => ([
+    ...algorithms,
+    { label, value }
+  ]), []);
+
+
+const encodingChoices: SelectChoices = Object.entries(hashEncodings)
+  .reduce((encodings, [label, value]) => ([
+    ...encodings,
+    { label, value }
+  ]), []);
+
+
 @Component({
   selector: 'c-hash-generator-page',
   templateUrl: './hash-generator-page.component.pug',
@@ -12,20 +27,9 @@ import { SelectChoices } from '../../controls/select/select.component';
 })
 export class HashGeneratorPageComponent {
 
-  public algorithms: SelectChoices = Object.entries(hashAlgorithms)
-    .reduce((algorithms, [label, value]) => ([
-      ...algorithms,
-      { label, value }
-    ]), []);
-
+  public algorithms: SelectChoices = algorithmChoices;
   public algorithm: HashAlgorithm = HashAlgorithm.SHA256;
-
-  public encodings: SelectChoices = Object.entries(hashEncodings)
-    .reduce((algorithms, [label, value]) => ([
-      ...algorithms,
-      { label, value }
-    ]), []);
-
+  public encodings: SelectChoices = encodingChoices;
   public encoding: HashEncoding = HashEncoding.Hex;
 
   public input: string = '';
