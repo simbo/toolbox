6a57a5ae9df12af317392e242770507ba6234552
Simon Lepel <simbo@simbo.de>
2018-07-01 02:16:09 +0200

    update karma and travis config to support chrome in ci

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -27,6 +27,13 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false
+    singleRun: false,
+    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    }
   });
 };
