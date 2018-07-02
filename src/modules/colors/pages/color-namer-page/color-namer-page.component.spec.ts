d98b4ed84f91b3298503f34d3f64d363e2c278df
Simon Lepel <simbo@simbo.de>
2018-07-02 02:29:39 +0200

    fix tests for color-namer page

--- src/modules/colors/pages/color-namer-page/color-namer-page.component.spec.ts
+++ src/modules/colors/pages/color-namer-page/color-namer-page.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { ColorNamerPageComponent } from './color-namer-page.component';
+import { StringsModule } from '../../../strings/strings.module';
 
 describe('ColorNamerPageComponent', () => {
   let component: ColorNamerPageComponent;
@@ -9,6 +10,9 @@ describe('ColorNamerPageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        StringsModule
+      ],
       declarations: [
         ColorNamerPageComponent
       ],
