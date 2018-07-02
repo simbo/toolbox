549943fa058973f4b508ba8158cecc3f2f81b731
Simon Lepel <simbo@simbo.de>
2018-07-02 02:26:42 +0200

    update color-namer template and styles

--- src/modules/colors/colors.module.ts
+++ src/modules/colors/colors.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { ControlsModule } from '../controls/controls.module';
 import { ColorsRoutingModule } from './colors-routing.module';
 import { ColorNamerPageComponent } from './pages/color-namer-page/color-namer-page.component';
+import { StringsModule } from '../strings/strings.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     ControlsModule,
+    StringsModule,
     ColorsRoutingModule
   ],
   declarations: [
