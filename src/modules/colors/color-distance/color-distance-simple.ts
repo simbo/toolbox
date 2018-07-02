a17f110e8131f80e6463eaf2d1b56c657dcd4c47
Simon Lepel <simbo@simbo.de>
2018-07-02 02:25:03 +0200

    optimize simple color distance calculation for a better delta value compared to other delta results

--- src/modules/colors/color-distance/color-distance-simple.ts
+++ src/modules/colors/color-distance/color-distance-simple.ts
@@ -3,5 +3,5 @@ export function colorDistanceSimple(colorA: number[], colorB: number[]): number 
     Math.abs(colorA[0] - colorB[0]) +
     Math.abs(colorA[1] - colorB[1]) +
     Math.abs(colorA[2] - colorB[2])
-  );
+  ) / 3 / 255 * 100;
 }
