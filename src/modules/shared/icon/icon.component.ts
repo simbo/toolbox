e05506d2142e4f8059707f3b995ebfc1c973318c
Simon Lepel <simbo@simbo.de>
2018-07-02 17:33:57 +0200

    let icon component throw an error if there is no symbol for given icon name; optimize icon template

--- src/modules/shared/icon/icon.component.ts
+++ src/modules/shared/icon/icon.component.ts
@@ -1,15 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ElementRef, AfterViewChecked } from '@angular/core';
 
 @Component({
   selector: 'c-icon',
   templateUrl: './icon.component.pug',
   styleUrls: ['./icon.component.scss']
 })
-export class IconComponent {
+export class IconComponent implements AfterViewChecked {
 
   @Input() public name: string = 'unicorn';
 
   @Input() public svgFilter: string;
+
+  private symbolChecked: boolean;
 
   public get svgSymbolLink(): string {
     return `#icon-symbol--${this.name}`;
@@ -17,6 +19,21 @@ export class IconComponent {
 
   public get svgFilterUrl(): string|false {
     return this.svgFilter ? `url(#${this.svgFilter})` : false;
+  }
+
+  constructor(
+    private elementRef: ElementRef
+  ) {}
+
+  public ngAfterViewChecked(): void {
+    if (this.symbolChecked === undefined) {
+      this.symbolChecked = (
+        this.elementRef.nativeElement as HTMLElement
+      ).querySelector(this.svgSymbolLink) ? true : false;
+      if (!this.symbolChecked) {
+        throw new Error(`'${this.name}' is not a valid icon name`);
+      }
+    }
   }
 
 }
