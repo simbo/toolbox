87e0dca9bd88d231a31231029d874ad9c34ae10a
Simon Lepel <simbo@simbo.de>
2018-07-12 00:32:27 +0200

    simplify icon component

--- src/modules/shared/icon/icon.component.ts
+++ src/modules/shared/icon/icon.component.ts
@@ -1,39 +1,14 @@
-import { Component, Input, ElementRef, AfterViewChecked } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'c-icon',
   templateUrl: './icon.component.pug',
   styleUrls: ['./icon.component.scss']
 })
-export class IconComponent implements AfterViewChecked {
+export class IconComponent {
 
-  @Input() public name: string = 'unicorn';
+  @Input() public name: string;
 
-  @Input() public svgFilter: string;
-
-  private symbolChecked: boolean;
-
-  public get svgSymbolLink(): string {
-    return `#icon-symbol--${this.name}`;
-  }
-
-  public get svgFilterUrl(): string|false {
-    return this.svgFilter ? `url(#${this.svgFilter})` : false;
-  }
-
-  constructor(
-    private elementRef: ElementRef
-  ) {}
-
-  public ngAfterViewChecked(): void {
-    if (this.symbolChecked === undefined) {
-      this.symbolChecked = (
-        this.elementRef.nativeElement as HTMLElement
-      ).querySelector(this.svgSymbolLink) ? true : false;
-      if (!this.symbolChecked) {
-        throw new Error(`'${this.name}' is not a valid icon name`);
-      }
-    }
-  }
+  constructor() {}
 
 }
