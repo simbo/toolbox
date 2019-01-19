8f4ae3d6d24bf12c84f668c8a9b8d1b1f3327714
Simon Lepel <simbo@simbo.de>
2019-01-19 01:34:01 +0100

    update workspace check script

--- bin/ensure-clean-workspace.sh
+++ bin/ensure-clean-workspace.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 if (find ./e2e -name "*.spec.ts" | xargs grep "describe.only\|it.only"); then
-  echo "\nYou have e2e tests focused with \".only\".\nPlease remove this before pushing.\n"
+  printf "\n\e[91mYou have e2e tests focused with \".only\".\nPlease remove this before pushing.\e[0m\n"
   exit 1
 fi
 
 if (find ./src -name "*.spec.ts" | xargs grep "fdescribe(\|fit("); then
-  echo "\nYou have unit tests focused with \"fdescribe\" or \"fit\".\nPlease remove this before pushing.\n"
+  printf "\n\e[91mYou have unit tests focused with \"fdescribe\" or \"fit\".\nPlease remove this before pushing.\e[0m\n"
   exit 1
 fi
 
 if ! [ -z "$(git status --porcelain)" ]; then
-  echo "\nYour working tree is dirty. Please commit or stash first.\n"
+  printf "\n\e[91mYour working tree is dirty. Please commit or stash first.\e[0m\n"
   exit 1
 fi
